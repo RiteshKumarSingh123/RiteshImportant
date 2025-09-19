c8774bc7a11f8faca5d35a3bb2d851ebf47af703
RiteshKumarSingh123 <riteshkumarsingh740@gmail.com>
2025-09-19 19:27:44 +0530

    Add files via upload

--- src/main/java/com/assh/supervisor/ServletInitializer.java
+++ src/main/java/com/assh/supervisor/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(SupervisorSignsInApplication.class);
+		return application.sources(SupervisorSignsIn3Application.class);
 	}
 
 }
---
+++ src/test/java/com/assh/supervisor/SupervisorSignsIn3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assh.supervisor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupervisorSignsIn3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
